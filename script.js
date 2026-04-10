78a5a6e788861ce33db51f56fdd1868106ae1f16
KonradowyPL <106955367+KonradowyPL@users.noreply.github.com>
2026-04-10 09:47:49 +0200

    filter counter

--- script.js
+++ script.js
@@ -1,99 +1,100 @@
-import data from './results.json' with { type: 'json' };
+import data from "./results.json" with { type: 'json' };
 
-const resultsEle = document.querySelector('.results')
-const filtersEle = document.querySelector('.filters')
+const resultsEle = document.querySelector(".results");
+const filtersEle = document.querySelector(".filters");
 
 const categories = (() => {
-  const categories = new Set()
-  Object.keys(data).forEach(q => {
-    console.log(q)
-    data[q].categories.forEach(a => {
-      categories.add(a)
+  const categories = new Set();
+  Object.keys(data).forEach((q) => {
+    console.log(q);
+    data[q].categories.forEach((a) => {
+      categories.add(a);
     });
   });
 
-  const asArr = Array.from(categories).sort()
-  console.log(asArr)
-  return asArr
+  const asArr = Array.from(categories).sort();
+  console.log(asArr);
+  return asArr;
 })();
 
-function updateParam () {
+function updateParam() {
   const filters = radio();
   const search = "";
   const url = new URL(window.location.href);
-  url.search = ''
+  url.search = "";
 
-  url.searchParams.set('search', search)
-  
-  Object.keys(filters).forEach(f => {
-    url.searchParams.set(f, filters[f])
-  }) 
+  url.searchParams.set("search", search);
 
-  window.history.replaceState({}, '', url);
+  Object.keys(filters).forEach((f) => {
+    url.searchParams.set(f, filters[f]);
+  });
 
-  generateHTML(data)
+  window.history.replaceState({}, "", url);
+
+  generateHTML(data);
 }
-
 
 {
   // generate categories
-  let html  = "<span></span>";
-  html  += "<span>Nie może zawierać</span>";
-  html  += "<span>Może zawierać</span>";
-  html  += "<span>Musi zawierać</span>";
-  categories.forEach(c => {
-    html += `<span>${c}</span>`
-    html += `<input type="radio" id="${c}-exclude" name="${c}" value="exclude" />`
-    html += `<input type="radio" id="${c}-unset" name="${c}" value="unset" checked/>`
-    html += `<input type="radio" id="${c}-include" name="${c}" value="include" />`
-  })
-  filtersEle.innerHTML = html
-  filtersEle.querySelectorAll('input').forEach(e => {
-    e.addEventListener("click", updateParam)
-  })
+  let html = "<span></span>";
+  html += "<span>Nie może zawierać</span>";
+  html += "<span>Może zawierać</span>";
+  html += "<span>Musi zawierać</span>";
+  categories.forEach((c) => {
+    html += `<span>${c} <span id=${c.replace("/", "_")}-counter></span></span>`;
+    html += `<input type="radio" id="${c}-exclude" name="${c}" value="exclude" />`;
+    html += `<input type="radio" id="${c}-unset" name="${c}" value="unset" checked/>`;
+    html += `<input type="radio" id="${c}-include" name="${c}" value="include" />`;
+  });
+  filtersEle.innerHTML = html;
+  filtersEle.querySelectorAll("input").forEach((e) => {
+    e.addEventListener("click", updateParam);
+  });
 }
 
-window.up = updateParam
-
+window.up = updateParam;
 
 function radio() {
-  const filters = {}
-  categories.forEach(c => {
-    const p = document.querySelector(`input[name="${c}"]:checked`).value
-    if (p !== 'unset') filters[c] = p
-  })
-  return filters
+  const filters = {};
+  categories.forEach((c) => {
+    const p = document.querySelector(`input[name="${c}"]:checked`).value;
+    if (p !== "unset") filters[c] = p;
+  });
+  return filters;
 }
 
-window.radio= radio;
-
+window.radio = radio;
 
 function filtered(question, filters, search) {
   // check filters
-  const q = data[question]
+  const q = data[question];
+
   // nie może zawierać
   for (const cat of q.categories) {
-    // console.log(cat)
     if (filters[cat] === "exclude") {
-      return true
+      return true;
     }
   }
 
   // musi zawierać
-  for (const cat of Object.keys(filters)) {
-    if (filters[cat] === "include" && !q.categories.includes(cat)) {
-
-      return true
+  for (const [cat, val] of Object.entries(filters)) {
+    if (val === "include" && !q.categories.includes(cat)) {
+      return true;
     }
   }
-  return false
-
+  return false;
 }
 
 function generateHTML(data) {
+  const counter = {};
+
+  for (const c of categories) {
+    counter[c] = 0;
+  }
+
   let html = "";
-  const filters = radio()
-  const search = ""
+  const filters = radio();
+  const search = "";
 
   for (const question in data) {
     const item = data[question];
@@ -102,22 +103,31 @@ function generateHTML(data) {
       continue;
     }
 
+    for (const cat of item.categories) {
+      counter[cat]++;
+    }
+
     html += `<div class="question">`;
     html += `<h3>${question}</h3>`;
     html += `<ul>`;
 
-    item.ans.forEach(answer => {
+    item.ans.forEach((answer) => {
       html += `<li>${answer}</li>`;
     });
 
     html += `</ul>`;
     html += `</div>`;
   }
-  resultsEle.innerHTML = html
+  resultsEle.innerHTML = html;
 
+  console.log(counter);
+
+  Object.keys(counter).forEach((f) => {
+    console.log(f);
+    document.querySelector(
+      `#${f.replace("/", "_")}-counter`
+    ).textContent = `(${counter[f]})`;
+  });
 }
 
-
-
-
-generateHTML(data)
+generateHTML(data);
